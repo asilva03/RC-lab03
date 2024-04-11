c1a71551fa71a65f6e51ee15524146671603c059
tiagocorreia321 <166047508+tiagocorreia321@users.noreply.github.com>
2024-04-11 15:24:05 +0100

    Update writenoncanonical.c

--- writenoncanonical.c
+++ writenoncanonical.c
@@ -5,6 +5,9 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #define BAUDRATE B38400
 #define MODEMDEVICE "/dev/ttyS1"
