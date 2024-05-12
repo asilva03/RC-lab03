b86bb8ce7e4fbf6516579e0fd60352afd24b2fb6
tiagocorreia321 <166047508+tiagocorreia321@users.noreply.github.com>
2024-05-12 15:59:44 +0100

    Create linklayer.h

---
+++ linklayer.h
@@ -0,0 +1,624 @@
+#ifndef LINKLAYER
+#define LINKLAYER
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+typedef struct linkLayer
+{
+    char serialPort[50];
+    int role; // defines the role of the program: 0==Transmitter, 1=Receiver
+    int baudRate;
+    int numTries;
+    int timeOut;
+} linkLayer;
+
+// ROLE
+#define NOT_DEFINED -1
+#define TRANSMITTER 0
+#define RECEIVER 1
+
+// SIZE of maximum acceptable payload; maximum number of bytes that application layer should send to link layer
+#define MAX_PAYLOAD_SIZE 1000
+
+// CONNECTION deafault values
+#define BAUDRATE_DEFAULT B38400
+#define MAX_RETRANSMISSIONS_DEFAULT 3
+#define TIMEOUT_DEFAULT 4
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+
+// MISC
+#define FALSE 0
+#define TRUE 1
+
+int fd, c, res;
+struct termios oldtio, newtio;
+size_t tam_dados;// Esta variável apenas é usada pelo transmissor
+int sum = 0, speed = 0;
+int role;
+
+// Opens a connection using the "port" parameters defined in struct linkLayer, returns "-1" on error and "1" on sucess
+int llopen(linkLayer connectionParameters);
+// Sends data in buf with size bufSize
+int llwrite(unsigned char *buf, int bufSize);
+// Receive data in packet
+int llread(unsigned char *packet);
+// Closes previously opened connection; if showStatistics==TRUE, link layer should print statistics in the console on close
+int llclose(linkLayer connectionParameters, int showStatistics);
+
+int llopen(linkLayer connectionParameters)
+{
+    fd = open(connectionParameters.serialPort, O_RDWR | O_NOCTTY);
+    if (fd < 0)
+    {
+        perror(connectionParameters.serialPort);
+        exit(-1);
+    }
+
+    // guarda os parametros associados a fd em oldtio
+    if (tcgetattr(fd, &oldtio) == -1)
+    { /* save current port settings */
+        perror("tcgetattr");
+        exit(-1);
+    }
+    if (connectionParameters.baudRate != BAUDRATE_DEFAULT)
+    {
+        perror("BAUDRATE not fixed");
+        exit(-1);
+    }
+
+    bzero(&newtio, sizeof(newtio));
+    newtio.c_cflag = BAUDRATE_DEFAULT | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+    newtio.c_lflag = 0;
+    /*
+    A configuração newtio.c_lflag = 0; desativa todos os modos de operação local do terminal.
+    isso inclui modos como ICANON (modo canônico) e ECHO (eco dos caracteres digitados).
+    Quando esses modos estão desativados, o terminal opera em modo não canônico, o que significa que a entrada é processada
+    imediatamente, caractere por caractere, sem esperar por uma nova linha ou qualquer outra interação do usuário.
+    Além disso, nenhum eco dos caracteres digitados é feito.
+    Isso é útil em situações em que se deseja um controle total sobre o processamento da entrada e saída do terminal.
+    */
+    newtio.c_cc[VTIME] = 0; /*  inter-character timer unused. Da 1s de timeout */
+    newtio.c_cc[VMIN] = 1;  /* blocking read until 5 chars received */
+
+    /*    newtio.c_lflag = 0;:
+           c_lflag é um membro da estrutura termios que controla os modos de operação local do terminal.
+           A configuração newtio.c_lflag = 0; desativa todos os modos de operação local do terminal.
+           Isso inclui modos como ICANON (modo canônico) e ECHO (eco dos caracteres digitados).
+
+       newtio.c_cc[VTIME] = 0;:
+           c_cc é um array que armazena os caracteres especiais do terminal, como controle de fluxo, caractere de final de linha, entre outros.
+           VTIME é uma constante que representa o índice do caractere de temporização de leitura.
+       newtio.c_cc[VTIME] = 0; define o temporizador de caractere como zero,
+           o que significa que não há temporização entre caracteres. Isso indica que a função read()
+           retornará imediatamente após ler o número mínimo de caracteres especificado pela configuração VMIN.
+
+       newtio.c_cc[VMIN] = 5;:
+           VMIN é uma constante que representa o índice do número mínimo de caracteres
+           para satisfazer uma chamada de leitura.
+       ewtio.c_cc[VMIN] = 5; configura o número mínimo de caracteres para 5.
+           Isso significa que a função read() bloqueará até que pelo menos 5 caracteres sejam recebidos antes de retornar.
+    */
+
+    /*
+    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
+    leitura do(s) próximo(s) caracter(es)
+    */
+
+    // limpa o buffer após escrever tudo
+    tcflush(fd, TCIOFLUSH);
+
+    // define os parametros associados
+    if (tcsetattr(fd, TCSANOW, &newtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+    /*Apartir deste momento, o terminal está corretamente configurado para manipulação dos dados
+     com as configurações da estrutura newtio*/
+    printf("New termios structure set\n");
+    role = connectionParameters.role;
+    unsigned char buffer;
+    int STOP = FALSE, STATE = 0, i = 0;
+
+    if (role == TRANSMITTER)
+    { // Só para o noncanonical no final porque refere-se ao envio do AK
+        i = 0;
+        while (i <= 4)
+        {
+
+            if (i == 0)
+                buffer = 0x5c;
+            if (i == 1)
+                buffer = 0x01;
+            if (i == 2)
+                buffer = 0x08;
+            if (i == 3)
+                buffer = 0x01 ^ 0x08;
+            if (i == 4)
+                buffer = 0x5c;
+            printf("%02x\n", buffer);
+
+            if (res = write(fd, &buffer, 1) < 0)
+            {
+                perror("Erro no envio do SET");
+                return -1;
+            }
+            buffer++;
+
+            i++;
+        }
+        printf("SET enviado com sucesso\n");
+    }
+
+    i = 0;
+    unsigned char XOR;
+    while (i <= 4)
+    {
+
+        res = read(fd, &buffer, 1); // Aqui, os programas encontra-se no estado 0 da máquina de estados a espera de receber alguma coisa do outro programa
+        printf("%d \n", res);
+        if (res < 0)
+        {
+            perror("Erro de leitura");
+            return -1;
+        }
+        printf("%02x\n", buffer);
+
+        if (STATE == 0)
+        {
+            printf("STATE: %d\n", STATE);
+            STATE++;
+        }
+
+        switch (STATE)
+        {
+        case 1:
+            printf("STATE: %d\n", STATE);
+            if (buffer == 0x5c)
+                break; // loop de flags 0x5c
+
+            if (role == TRANSMITTER)
+            {
+                if (buffer == 0x03)
+                    STATE++;
+                else
+                    STATE = 0;
+            }
+
+            if (role == RECEIVER)
+            {
+                if (buffer == 0x01)
+                    STATE++;
+                else
+                    STATE = 0;
+            }
+            break;
+
+        case 2:
+            printf("STATE: %d\n", STATE);
+            if (role == TRANSMITTER)
+            {
+                if (buffer == 0x06)
+                {
+                    STATE = 3;
+                    break;
+                }
+            }
+
+            if (role == RECEIVER)
+            {
+                if (buffer == 0x08)
+                {
+                    STATE = 3;
+                    break;
+                }
+            }
+
+            if (buffer == 0x5c)
+                STATE = 1;
+            else
+                STATE = 0;
+            break;
+
+        case 3:
+            printf("STATE: %d\n", STATE);
+            if (role == TRANSMITTER)
+            {
+                XOR = 0x03 ^ 0x06;
+                if (buffer == XOR)
+                {
+                    STATE = 4;
+                    break;
+                }
+            }
+
+            if (role == RECEIVER)
+            {
+                XOR = 0x01 ^ 0x08;
+                if (buffer == XOR)
+                {
+                    STATE = 4;
+                    break;
+                }
+            }
+
+            if (buffer == 0x5c)
+                STATE = 1;
+            else
+                STATE = 0;
+            break;
+
+        case 4:
+            printf("STATE: %d\n", STATE);
+
+            if (buffer == 0x5c)
+                STOP = TRUE;
+            else
+                STATE = 0;
+            break;
+
+        default:
+            break;
+        }
+        i++;
+    }
+
+    if (STOP == TRUE && role == RECEIVER)
+    { // Só para o noncanonical no final porque refere-se ao envio do UA
+        i = 0;
+        while (i <= 4)
+        {
+
+            if (i == 0)
+                buffer = 0x5c;
+            if (i == 1)
+                buffer = 0x03;
+            if (i == 2)
+                buffer = 0x06;
+            if (i == 3)
+                buffer = 0x03 ^ 0x06;
+            if (i == 4)
+                buffer = 0x5c;
+            printf("%02x\n", buffer);
+
+            if (res = write(fd, &buffer, 1) < 0)
+            {
+                perror("Erro no envio do UA");
+                return -1;
+            }
+            i++;
+        }
+        printf("UA enviado com sucesso\n");
+    }
+    else if (role == RECEIVER)
+    {
+        printf("UA não enviado\n");
+    }
+    if (STOP == TRUE && role == TRANSMITTER)
+        printf("UA recebido com sucesso\n");
+    else if (role == TRANSMITTER)
+        printf("UA não recebido\n");
+
+    /*o terminal volta as configurações originais devido ao facto de certas aplicações
+      não definem as proprias configurações de terminal, por isso é importante voltar as definições originais*/
+    return 1;
+
+} /*while(sum <= bufSize){//sum serve para contar os bytes enviados para o recetor quando chegar ao MAX_PAYLOAD_SIZE, para
+     unsigned char frase[] = "Helicopter Helicopter";/*temos que modular o tamanho de envio dos dados? Então eu tenho que criar um
+     vetor auxiliar que envie os dados apartir do endereço recebido?*/
+// bufSsize é o número de bytes total a ser enviados  e recebidos pela função llread*/
+int llwrite(unsigned char *buf, int bufSize)
+{
+    int i = 0, j = 0; // i representa a posição do array aux_buf antes de calcular BCC2, j representa a posição de aux_buf para BCC2 e 0x5c
+    unsigned char aux_buf[bufSize], BCC2;
+    aux_buf[0] = 0x5c;
+    aux_buf[1] = 0x01;
+    aux_buf[2] = 0x08;
+    aux_buf[3] = 0x01 ^ 0x08;
+    printf("tam dados:%zu\n",tam_dados);
+
+    while (i < (tam_dados + 6) && i < bufSize) // verificar a soma de 4
+    {
+
+        if (i <= 3)
+        {
+            if (write(fd, &aux_buf[i], 1) < 0)
+            {
+                perror("Erro no envio do packet");
+                return -1;
+            }
+            printf("%02x \n", aux_buf[i]);
+        }
+        else if ((i < (bufSize - 2)) && (i < (tam_dados + 3)))
+        // tam_dados + 3 porque: tam_dados + 4, trabalha-se com aux_buf[] depois do cabeçalho, -1 porque i representa a posição do array aux_buf[i]  
+        // Esta ultima condição serve quando bufSize > (tam_dados + 6)
+        {
+            if (write(fd, buf, 1) < 0)
+            {
+
+                perror("Erro de envio do packet\n");
+                return -1;
+            }
+            aux_buf[i] = *buf;
+            printf("%c", *buf);
+            buf++;
+            printf("Valor do i:%d\n", i);
+        }
+        else if (i <= (bufSize - 2) && (i <= (tam_dados + 3))) //28 != 25 e 
+        {
+            j = 4; // Posição do aux_buf onde os dados começam a ser guardados
+            BCC2 = aux_buf[j];
+            printf("Antes de enviar\n");
+            printf("O valor atual de BCC2:%02x\n", BCC2);
+            while (j < (bufSize - 2) && (j < (tam_dados + 2))){
+                /* Para tam_dados = 22, 22 + 4 -1 = 25,este valor corresponde á posição do array aux_buf que contém o ultimo byte de dados
+                   , o que equivale a tam_dados + 3 = 25, mas como se utiliza aux_buf[j + 1],fica tam_dados + 2 = 24, e como já se faz a
+                   primeira operação de XOR, é preciso decrementar ainda mais um, o que equivale a passar de j <= (tam_dados + 2)
+                   para j < (tam_dados + 2)*/ 
+                printf("%c\n",aux_buf[j + 1]);
+                    BCC2 = BCC2 ^ aux_buf[j + 1];
+                    printf("O valor atual de BCC2:%02x\n", BCC2); // -3 porque, para um bufSize = 10 como exemplo, 10-3 = 7, 7+1 que corresponde
+                j++;                              // a aux_buf[j + 1] ou seja é a posição 8 que é a posição onde BCC2 será guardado. A ultima posição do vetor seria 9.
+            }
+            printf("Valor do BCC2:%02x\n", BCC2);
+            aux_buf[j] = BCC2;
+            printf("Enviei BCC2\n");
+            if (write(fd, &aux_buf[j], 1) < 0)
+            {
+                perror("Erro de envio do packet\n");
+                return -1;
+            }
+        }
+        else
+        {
+            printf("Enviei a flag de terminação\n");
+            printf("Valor do aux_buf antes de enviar a flag de terminação:%02x\n", aux_buf[j]);
+            aux_buf[j + 1] = 0x5c;
+            if (write(fd, &aux_buf[j + 1], 1) < 0)
+            {
+                perror("Erro de envio do packet\n");
+                return -1;
+            }
+        }
+        i++;
+    }
+    return i;
+}
+/*llread depois de feito o handshake, vai ficar a espera de receber os dados, o que quer dizer ao mesmo tempo que se está enviar um byte
+, estará-se a receber o mesmo no outro lado, sistema FIFO. O envio pode ser mais rapida do que a receção, não origina erros?*/
+int llread(unsigned char *packet)
+{
+    //Usa-se alocação dinâmica porque não se sabe o tamanho do packet que vai chegar
+
+    unsigned char *aux_packet = packet, BCC1, BCC2;
+    unsigned char *bufr = (unsigned char *)malloc(sizeof(unsigned char));
+    unsigned char *aux_bufr;
+    int STOP = FALSE, STATE = 0, i = 0;
+    while (STOP == FALSE)
+    {
+
+        if (STATE < 4) // Para não chamar a função read() ao ir para o estado da obtenção do XOR2
+        {
+            if (i != 0)
+            {
+                printf("Vou realocar memória, valor do i:%d\n", i);
+                unsigned char *aux_bufr = realloc(bufr, (i + 1) * sizeof(unsigned char));
+                if (aux_bufr == NULL)
+                {
+
+                    printf("Erro: Falha ao realocar memória.\n");
+                    free(aux_bufr);
+                    return -1;
+                }
+                else
+                {
+
+                    bufr = aux_bufr;
+                }
+            }
+            res = read(fd, &bufr[i], 1);
+            printf("Valor da primeira posição de bufr:%02x\n", bufr[i]);
+            if (res < 0)
+            {
+                perror("Erro de leitura dos dados\n");
+                return -1;
+            }
+        }
+
+        if (STATE == 0)
+        {
+            printf("STATE: %d\n", STATE);
+            STATE++;
+        }
+
+        switch (STATE)
+        {
+        case 1:
+            printf("STATE: %d\n", STATE);
+
+            printf("%02x\n", bufr[i]);
+            if (bufr[i] == 0x5c)
+            {
+                i++;
+                break; // loop de flags 0x5c
+            }
+            if (role == TRANSMITTER)
+            {
+                if (bufr[i] == 0x03)
+                {
+                    STATE++;
+                    i++;
+                }
+                else
+                {
+                    STATE = 0;
+                    i++;
+                }
+            }
+
+            if (role == RECEIVER)
+            {
+                if (bufr[i] == 0x01)
+                {
+                    STATE++;
+                    i++;
+                }
+                else
+                {
+                    STATE = 0;
+                    i++;
+                }
+            }
+            break;
+
+        case 2:
+            printf("STATE: %d\n", STATE);
+            printf("%02x\n", bufr[i]);
+            printf("%c\n", bufr[i]);
+            if (role == TRANSMITTER)
+            {
+                if (bufr[i] == 0x06)
+                {
+                    STATE = 3;
+                    i++;
+                    break;
+                }
+            }
+
+            if (role == RECEIVER)
+            {
+                if (bufr[i] == 0x08)
+                {
+                    STATE = 3;
+                    i++;
+                    break;
+                }
+            }
+
+            if (bufr[i] == 0x5c)
+            {
+                STATE = 1;
+                i++;
+            }
+            else
+            {
+                STATE = 0;
+                i++;
+            }
+            break;
+
+        case 3:
+            printf("STATE: %d\n", STATE);
+            printf("%02x\n", bufr[i]);
+            printf("%c\n", bufr[i]);
+            if (role == TRANSMITTER)
+            {
+                BCC1 = 0x03 ^ 0x06;
+                if (bufr[i] == BCC1)
+                {
+                    STATE = 4;
+                    i++;
+                    break;
+                }
+            }
+
+            if (role == RECEIVER)
+            {
+                BCC1 = 0x01 ^ 0x08;
+                if (bufr[i] == BCC1)
+                {
+                    STATE = 4;
+                    i++;
+                    break;
+                }
+            }
+
+            if (bufr[i] == 0x5c)
+            {
+                STATE = 1;
+                i++;
+            }
+            else
+            {
+                STATE = 0;
+                i++;
+            }
+            break;
+
+        case 4:
+            printf("STATE: %d\n", STATE);
+            // printf("%c buffer\n",bufr);
+            while (1)
+            {
+                
+                unsigned char *aux_bufr = realloc(bufr, (i + 1) * sizeof(unsigned char));
+                if (aux_bufr == NULL)
+                {
+
+                    printf("Erro: Falha ao realocar memória.\n");
+                    free(aux_bufr);
+                    return -1;
+                }
+                else
+                {
+
+                    bufr = aux_bufr;
+                }
+                // guarda no packet antes da leitura porque ao voltar para o inicio do primeiro while, ocorre a chamada da função read()
+                read(fd, &bufr[i], 1);
+                printf("%c valor de i:%d\n", bufr[i], i);
+                if (bufr[i] == 0x5c) break; //flag final do packet
+                i++;
+            }
+            printf(" guardado no buffer com sucesso\n");
+            STATE = 5;
+            break;
+        case 5:
+            printf("STATE: %d\n", STATE);
+            printf("Dados do buffer:");
+            printf("valor do i:%d\n", i);
+            for (int j = 4; j < (i - 1); j++) // -1 para não inclui BCC2
+            {                                 // bufr[i] = 0x5c neste momento
+            printf("%c",bufr[j]);
+
+                BCC2 = BCC2 ^ bufr[j];
+                printf("Valor atual de BCC2:%02x\n", BCC2);
+            }
+            printf("Valor em hexadecimal de BCC2:%02x e de bufr[i - 1]:%02x", BCC2, bufr[i - 1]);
+            if (BCC2 == bufr[i - 1])
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    *packet = bufr[j];
+                    if (j != i)
+                        packet++;
+                    printf("%c", *packet);
+                }
+                STOP = TRUE;
+                free(bufr); 
+                printf(" Packet recebido com sucesso\n");
+            }
+            else
+            {
+                printf(" Erro na receção do packet\n");
+                return -1;
+            }
+            break;
+
+        default:
+            break;
+        }
+    }
+    return 1;
+}
+#endif
